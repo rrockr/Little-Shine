b1609fa78343c06459901cb3653b053aabc91f0b
Darius Tan Tee Hao <rockmanexexe@hotmail.com>
2016-08-10 23:49:06 +0800

    Game Complete.

---
+++ Assets/Scripts/ExitPoint.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ExitPoint : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.tag == "Player")
+        {
+            SceneManager.LoadScene("Scenes/GameEnd");
+        }
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Menu : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("Scenes/Main");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Scenes/MainMenu");
+    }
+}
